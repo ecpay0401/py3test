b2c9ae985c018e8bb22f7c4db21a532af6c5b7f8
Adan <adanyao@gmail.com>
2022-09-10 00:59:49 +0800

    09100100

---
+++ python3/knowlist.py
@@ -0,0 +1,11 @@
+james = [23,19,22,31,18]
+print("列印james串列",james)
+james=['Lebron james',23,19,22,31,18]
+print("列印james串列",james)
+fruits =['apple','banana','orange']
+print("列印fruint串列",fruits)
+cfruits=['蘋果','香焦','舉子']
+print("列印cfruits串列",cfruits)
+ielts = [5.5,6.0,6.5]
+print("列印IELTS成績",ielts)
+print("串列james資料型態是: ",type(james))
